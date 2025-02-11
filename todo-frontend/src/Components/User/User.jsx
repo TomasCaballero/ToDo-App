93930168afc25301aeb5dd1a4eb62d36f507b335
Tomas Bautista Caballero <caballerotomas97@gmail.com>
2025-02-11 10:13:44 -0300

    Funcion crear Tarea

--- todo-frontend/src/Components/User/User.jsx
+++ todo-frontend/src/Components/User/User.jsx
@@ -3,20 +3,86 @@ import "./UserStyle.css"
 
 const User = () => {
     const [tasks, setTasks] = useState([]);
+    const [modal, setModal] = useState(false);
     
     useEffect(()=>{
         let tasksLocalStorage = JSON.parse(localStorage.getItem("user")).tasks;
         console.log(tasksLocalStorage);
         setTasks(tasksLocalStorage);
         console.log(tasks.length);
-    },[])
-  return (
-    <>
+    },[]);
+
+    const abrirModal = (boolean) => {
+        setModal(boolean); // Cambia el estado del modal (abrir o cerrar)
+    };
+
+    const crearTarea = (e) =>{
+        e.preventDefault();
+        let title = e.target.title.value;
+        let content = e.target.content.value;
+        
+        if(title && content){
+            console.log(JSON.parse(localStorage.getItem("user")).id);
+            let task = {
+                user:{
+                    id: JSON.parse(localStorage.getItem("user")).id
+                },
+                title: title,
+                date: obtenerFechaActual(),
+                content: content
+            }
+            guardarTaskEnBaseDeDatos(task);
+            abrirModal(false)
+        }else{
+            alert("Ambos campos son obligatorios");
+        }
+    }
+
+    const obtenerFechaActual = () => {
+        const fecha = new Date();
+        const año = fecha.getFullYear();
+        const mes = String(fecha.getMonth() + 1).padStart(2, '0');
+        const dia = String(fecha.getDate()).padStart(2, '0');
     
+        return `${año}-${mes}-${dia}`;
+    };
+
+    const guardarTaskEnBaseDeDatos = async (newTask) => {
+        try {
+          const response = await fetch("/api/tasks", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTask),
+          });
+      
+          if (response.ok) {
+            const task = await response.json();
+            alert(`Tarea creada con éxito: ${task.title}`);
+            
+            setTasks((prevTasks) => [...prevTasks, task]);
+          } else {
+            alert("Hubo un error al crear la tarea.");
+          }
+        } catch (error) {
+          console.error("Error al realizar la solicitud POST:", error);
+          alert("No se pudo completar la solicitud");
+        }
+    };
+
+
+      
+    
+  
+  
+  
+    return (
+    <>  
     <div className="userContainer">
         <div className="tasksHeader">
             <p>Mis Tareas:</p>
-            <button>Nueva Tarea</button>
+            <button onClick={() => abrirModal(true)}>Nueva Tarea</button>
         </div>
         <div className="tasksContainer">
             {
@@ -44,6 +110,19 @@ const User = () => {
         </div>
     </div>
     
+    <div className={modal === false ? "nuevaTareaModal modal modalClose" : "nuevaTareaModal modal"}>
+        <div className="modalHeader">
+            <p>Nueva tarea:</p>
+            <button className="btnCerrar" onClick={() => abrirModal(false)}>X</button>
+        </div>
+        <div className="modalContent">
+            <form onSubmit={crearTarea} className="modalContent">
+                <input type="text" name="title" placeholder="Ingrese el título" />
+                <input type="text" name="content" placeholder="Ingrese el contenido" />
+                <button type="submit">Crear</button>
+            </form>
+        </div>
+    </div>
     </>
   )
 }
